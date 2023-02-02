3826f68cf5ff5499f944347e3dfb49704f709b89
3FBB6CE6DBACED47B8BFCF0F661B6237 <3FBB6CE6DBACED47B8BFCF0F661B6237@i-search.com.cn>
2023-02-02 20:06:12 +0800

    2-8 增加自定义配置示例

--- src/main/java/com/wanghao/wiki/controller/TestController.java
+++ src/main/java/com/wanghao/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.wanghao.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 }
