c25f920ca62f61f47a54e72eed05c29589462391
3FBB6CE6DBACED47B8BFCF0F661B6237 <3FBB6CE6DBACED47B8BFCF0F661B6237@i-search.com.cn>
2023-02-05 10:50:27 +0800

    3-6 完成电子书查询接口

---
+++ src/main/java/com/wanghao/wiki/controller/EbookController.java
@@ -0,0 +1,21 @@
+package com.wanghao.wiki.controller;
+
+import com.wanghao.wiki.domain.Ebook;
+import com.wanghao.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){return ebookService.list();}
+}
