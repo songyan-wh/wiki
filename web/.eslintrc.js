9e84dbef54c77894053c566426c925a6783c7867
3FBB6CE6DBACED47B8BFCF0F661B6237 <3FBB6CE6DBACED47B8BFCF0F661B6237@i-search.com.cn>
2023-02-07 08:58:08 +0800

    5-2 使用axios调用后端接口/ebook/list

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-componets': 'off',
+    'vue/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   }
 }
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    WIKI
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "the-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
